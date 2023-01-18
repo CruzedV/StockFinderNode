e5ed5cdc026651046b9d9d6170ccb2e53346fc6e
CruzedV <mr.dobrikv@mail.com>
2023-01-18 11:16:02 +0300

    Better structure. Installed tinkoff-invest-js

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { join } from 'path';
-import { Transport } from '@nestjs/microservices';
 
-const app = NestFactory.createMicroservice(AppModule, {
-  transport: Transport.GRPC,
-  options: {
-    package: 'hero',
-    protoPath: join(__dirname, 'Proto/*.proto'),
-  },
-});
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap();
