850ed6b2714c8b2d5bd1d736855ca427e9c56571
CruzedV <mr.dobrikv@mail.com>
2023-01-28 10:52:03 +0300

    Navbar ready

--- frontend/src/Components/Navbar/Navbar.tsx
+++ frontend/src/Components/Navbar/Navbar.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
-import { Typography, Box, Toolbar, Button } from '@mui/material';
+import { Typography, Box, Toolbar, Button, ButtonGroup, IconButton } from '@mui/material';
+// Acciunt Icon
 import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
+// Buy
 import AddShoppingCartRoundedIcon from '@mui/icons-material/AddShoppingCartRounded';
+// Portfolio
 import CardTravelRoundedIcon from '@mui/icons-material/CardTravelRounded';
+// Settings
+import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
 
 export class Navbar extends React.Component {
   render() {
@@ -11,13 +16,54 @@ export class Navbar extends React.Component {
       <Box>
         <AppBar position="static" sx={{borderRadius: "0.9em"}}>
           <Toolbar>
-            <Typography>
-              STOCKFINDER
-            </Typography>
-            {/* Router */}
-            <Box>
-              <Button sx={{startIcon: "../../resources/Loopa.png"}}>
-              </Button>
+            {/* Brand */}
+            <Box sx={{
+              width: "11em"
+            }}>
+              <Typography sx={{
+                textAlign: "center"
+              }}>
+                STOCKFINDER
+              </Typography>
+            </Box>
+            {/* Router Host*/}
+            <Box sx={{
+              ml: "auto",
+              mr: "auto"
+            }}>
+              <ButtonGroup>
+                {/* Buy */}
+                <Button startIcon={<AddShoppingCartRoundedIcon/>} sx={{
+                  color: "text.primary"
+                }}>
+                  <Typography color="text.primary">Купить</Typography>
+                </Button>
+                {/* Portfolio */}
+                <Button startIcon={<CardTravelRoundedIcon/>} sx={{
+                  color: "text.primary"
+                }}>
+                  <Typography color="text.primary">Портфель</Typography>
+                </Button>
+                {/* Settings */}
+                <Button startIcon={<SettingsRoundedIcon/>} sx={{
+                  color: "text.primary"
+                }}>
+                  <Typography color="text.primary">Настройки</Typography>
+                </Button>
+              </ButtonGroup>
+            </Box>
+            {/* Account */}
+            <Box sx={{
+              width: "11em"
+            }}>
+              <IconButton sx={{
+                  color: "text.primary",
+                  ml: "auto",
+                  mr: "auto",
+                  display: "block"
+                }}>
+                <AccountCircleRoundedIcon/>
+              </IconButton>
             </Box>
           </Toolbar>
         </AppBar>
