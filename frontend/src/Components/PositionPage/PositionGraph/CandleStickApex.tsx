70a586c0bac7d489556c7348e27c691a5e5c9080
CruzedV <mr.dobrikv@mail.com>
2023-02-06 10:26:24 +0300

    Graph done

--- frontend/src/Components/PositionPage/PositionGraph/CandleStickApex.tsx
+++ frontend/src/Components/PositionPage/PositionGraph/CandleStickApex.tsx
@@ -9,10 +9,12 @@ export class CandleStickApex extends React.Component<{}, {options:any, series:an
     this.state = {
       options: {
         chart: {
-          id: 'candlestick'
+          id: 'candlestick',
+          foreColor: '#F4F4F4',
+          fontFamily: 'Inter',
         },
         xaxis: {
-          categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+          categories: [2016, 2017, 2018, 2019, 2020, 2021, 2022]
         },
         plotOptions: {
           candlestick: {
@@ -22,6 +24,13 @@ export class CandleStickApex extends React.Component<{}, {options:any, series:an
             }
           }
         },
+        tooltip: {
+          enabled: true,
+          style: {
+            fontSize: '12px',
+            fontFamily: 'Inter',
+          },
+        },
         title: {
           text: 'Показатели ОФЗ 12454566534343',
           align: 'left'
@@ -29,31 +38,31 @@ export class CandleStickApex extends React.Component<{}, {options:any, series:an
       },
       series: [{
         data: [{
-          x: new Date(2016, 1, 1),
+          x: 2016,
           y: [51.98, 56.29, 51.59, 53.85]
         },
         {
-          x: new Date(2016, 1, 1),
+          x: 2017,
           y: [51.98, 56.29, 51.59, 53.85]
         },
         {
-          x: new Date(2016, 1, 1),
+          x: 2018,
           y: [51.98, 56.29, 51.59, 53.85]
         },
         {
-          x: new Date(2016, 1, 1),
+          x: 2019,
           y: [51.98, 56.29, 51.59, 53.85]
         },
         {
-          x: new Date(2016, 1, 1),
+          x: 2020,
           y: [51.98, 56.29, 51.59, 53.85]
         },
         {
-          x: new Date(2016, 1, 1),
+          x: 2021,
           y: [51.98, 56.29, 51.59, 53.85]
         },
         {
-          x: new Date(2016, 8, 1),
+          x: 2022,
           y: [52.76, 57.35, 52.15, 57.03]
         }]
       }],
